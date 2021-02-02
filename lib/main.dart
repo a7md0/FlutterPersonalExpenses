c90eba68cf051887f382bfd46741a09f2b062c9d
a7md0 <a7m3d699@gmail.com>
2021-02-02 14:39:03 +0300

    Add accent color

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
       ),
       home: MyHomePage(),
     );

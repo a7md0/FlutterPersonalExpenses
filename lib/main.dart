d23ee952dfddabf5fcfc7e61c2d820a90bcad888
a7md0 <a7m3d699@gmail.com>
2021-02-02 14:13:53 +0300

    Add + btn to appBar

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
